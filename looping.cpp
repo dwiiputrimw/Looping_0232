4656161ba71e4ba369074e9e33fda0bd3eb8a2f0
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-03-30 18:54:41 +0700

    Menambahkan Logika AND

--- looping.cpp
+++ looping.cpp
@@ -67,4 +67,19 @@ int main(){
         status = "Tidak lulus";
 
     cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
+
+      //Logika AND
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if ((rerata >= 60) && (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
 } 
